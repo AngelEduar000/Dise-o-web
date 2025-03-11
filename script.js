aa28ee36137a79d7437eaa04da92d1cb26fa2f09
unisebastian <smayorga5@udi.edu.co>
2025-03-10 23:53:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n){
+    showSlides(slideIndex = n);
+}
+
+
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+}
+
+//Auto Slide
+
+var autoSlideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    autoSlideIndex++;
+    if (autoSlideIndex > slides.length) { autoSlideIndex = 1 }
+
+    slides[autoSlideIndex - 1].style.display = "block";
+    setTimeout(autoShowSlides, 4000); // Cambia cada 5 segundos
+}
